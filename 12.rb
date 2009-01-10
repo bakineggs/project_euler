a5ebe9efc0f82e8737917005d9034ded6d324a24
Dan Barry <dan@bakineggs.com>
2009-01-10 03:20:12 -0600

    not as legible, but this should be faster

--- 12.rb
+++ 12.rb
@@ -9,9 +9,16 @@ class Fixnum
   end
 
   def factors
-    @factors ||= (1..self.abs).select do |number|
-      self % number == 0
+    raise NotImplementedError if self <= 0
+    return @factors unless @factors.nil?
+    @factors = [1, self]
+    if smallest_other_factor = ((1..Math.sqrt(self)).collect-@factors).find{|number| self % number == 0}
+      @factors.push smallest_other_factor
+      largest_other_factor = self / smallest_other_factor
+      @factors += largest_other_factor.factors
+      @factors += largest_other_factor.factors.map{|factor| factor * smallest_other_factor}
     end
+    @factors = @factors.uniq
   end
 end
 
